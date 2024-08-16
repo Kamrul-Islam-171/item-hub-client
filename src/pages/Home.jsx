f77709e8ffd8ebe84cdbc9b1c16ff9fee86fec23
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-08-16 18:26:20 +0600

    search done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { ScaleLoader } from "react-spinners";
 import ProductCard from "../components/ProductCard";
 import { useEffect, useState } from "react";
 import { GrCaretNext, GrCaretPrevious } from "react-icons/gr";
+import { FaArrowRightLong } from "react-icons/fa6";
+
 
 
 const Home = () => {
@@ -14,9 +16,12 @@ const Home = () => {
     const [limit] = useState(10);
     const [count, setCount] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
+    const [search, SetSearch] = useState('');
     const itemsPerpage = 10;
     const numberOfPages = Math.ceil(count / itemsPerpage);
     const pages = [];
+
+
 
     for (let x = 1; x <= numberOfPages; x++) pages.push(x);
     // console.log(pages)
@@ -31,15 +36,15 @@ const Home = () => {
     // })
 
     useEffect(() => {
-        axiosPublic.get(`/docCount`)
+        axiosPublic.get(`/docCount?search=${search}`)
         .then(data => setCount(data.data.length))
-    }, [])
+    }, [search])
 
 
     const { data: products = [], isLoading, refetch } = useQuery({
-        queryKey: ['all-products', page, limit],
+        queryKey: ['all-products', page, limit, search],
         queryFn: async () => {
-            const { data } = await axiosPublic.get(`/products?page=${page}&limit=${limit}`);
+            const { data } = await axiosPublic.get(`/products?page=${page}&limit=${limit}&search=${search}`);
             // setCount(data.length);
             return data
         }
@@ -52,6 +57,14 @@ const Home = () => {
         // refetch()
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log(e.target.search.value)
+        SetSearch(e.target.search.value);
+    }
+
+    console.log(search)
+
     if (isLoading) {
         <div>
             <div className="flex justify-center items-center h-screen">
@@ -62,8 +75,17 @@ const Home = () => {
     return (
         <div className="mt-[100px] container mx-auto">
 
-
             <div>
+                <form onSubmit={handleSubmit} className="flex flex-col md:flex-row  items-center justify-center gap-5">
+                    <div>
+                        <input name="search" className="w-full py-3 px-5 border-teal-300 border outline-none" type="text" placeholder="Search by product name" />
+                    </div>
+                    <button className="btn bg-teal-500 border-none text-white text-lg hover:bg-teal-800">Go <FaArrowRightLong /></button>
+                </form>
+            </div>
+
+
+            <div className="pt-10">
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
                     {
                         products?.map(product => <ProductCard key={product._id} product={product}></ProductCard>)
